feb7609d4dd2c7c80d0e6db19df92064ecb8426e
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-18 03:15:27 +0300

    01. Struct

---
+++ Demo_OOP01/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Demo_OOP01
+{
+    internal struct Point
+    {
+        public int X;
+        public int Y;
+
+        //Constructor: Special Methods
+        //             1. Named Like Struct
+        //             2. Has No Return Type
+
+
+        //CLR :will Generate Paremeterless Constructor
+        // This Constructor Will Initialized The Attributes With The Default
+
+        //Constructor
+        public Point()
+        {
+
+        }
+
+        //public Point()
+        //{
+        //    X = 8;
+        //    Y = 16;
+        //}
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+        public void PrintPoint()
+        {
+            Console.WriteLine($"({X}, {Y})");
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+    }
+}
--- Demo_OOP01/Program.cs
+++ Demo_OOP01/Program.cs
@@ -1,9 +1,60 @@
 ﻿namespace Demo_OOP01
 {
+    /*
+     * Class
+     * Struct
+     * Interface
+     * Enum
+     */
     internal class Program
     {
         static void Main()
         {
+            #region 01. Struct
+            ////Struct: Value Types: STACK
+
+
+            ////Employee
+            ////Id, name, age, address
+
+            //// Point
+
+
+            //Point P01;
+
+            //// Declare For Object From Type 'Point '
+            //// POI: Object
+            ////Allocate UnInitialized 8 Bytes at STACK
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+
+            ////P01.X = 12;
+            ////P01.Y = 34;
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            //P01 = new Point(1, 2);
+            ////new : Used For Select The Constructor
+
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            ////Console.WriteLine(P01);//Demo_OOP01.Point
+            ////Console.WriteLine(P01);//Demo_OOP01.Point
+            //P01.PrintPoint();
+
+            //Console.WriteLine(P01.ToString());//Demo_OOP01.Point
+            //Console.WriteLine(P01.ToString());//(1,2) -> After Overrides
+
+            //Console.WriteLine(P01);//(1,2) 
+            #endregion
+
+
+
+
 
         }
     }
