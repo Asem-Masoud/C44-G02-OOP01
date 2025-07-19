abbb2701e8444083a45643455c2a084edb178b1d
عاصم محمد محمد يوسف مسعود <125200754@sha.edu.eg>
2025-07-20 00:37:38 +0300

    Q1P01

--- Demo_OOP01/Program.cs
+++ Demo_OOP01/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Demo_OOP01
+﻿using System;
+
+namespace Demo_OOP01
 {
     internal class Program
     {
+
         static void Main()
         {
+            #region Q1P01
 
+            //1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+            //Write a C# program that takes two points as input from the user and calculates the distance between them. 
+
+
+            //Console.WriteLine("Enter coordinates for the first point:");
+            //Console.Write("X1: ");
+            //double x1 = double.Parse(Console.ReadLine());
+            //Console.Write("Y1: ");
+            //double y1 = double.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\nEnter coordinates for the second point:");
+            //Console.Write("X2: ");
+            //double x2 = double.Parse(Console.ReadLine());
+            //Console.Write("Y2: ");
+            //double y2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter first point (X Y):");
+            string[] parts1 = Console.ReadLine().Split();
+            int x1 = Convert.ToInt32(parts1[0]);
+            int y1 = Convert.ToInt32(parts1[1]);
+
+            Console.WriteLine("Enter second point (X Y):");
+            string[] parts2 = Console.ReadLine().Split();
+            int x2 = Convert.ToInt32(parts2[0]);
+            int y2 = Convert.ToInt32(parts2[1]);
+
+            Point point1 = new Point(x1, y1);
+            Point point2 = new Point(x2, y2);
+
+            double distance = point1.DistanceTo(point2);
+
+            Console.WriteLine($"\nPoint 1: {point1}");
+            Console.WriteLine($"Point 2: {point2}");
+            Console.WriteLine($"Distance between the points: {distance:F2}");
+
+
+
+            #endregion
         }
     }
 }
